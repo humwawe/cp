9ee6894197f6d9815f5d79798d88bd7a15b1aa3b
hum <wawe8963098@163.com>
2025-03-11 21:46:19 +0800

    c439

--- src/leetcode/c439/p4.cpp
+++ src/leetcode/c439/p4.cpp
@@ -1,2 +1,41 @@
 #include <lc-util.h>
 
+class Solution {
+public:
+  string generateString(string str1, string str2) {
+    int n = str1.size(), m = str2.size();
+    string ans;
+    ans.resize(n + m - 1);
+
+    vector<bool> flag(n + m - 1);
+    for (int i = 0; i < n; i++) {
+      if (str1[i] == 'T') {
+        for (int j = 0; j < m; j++)
+          ans[i + j] = str2[j], flag[i + j] = true;
+      }
+    }
+
+    for (int i = 0; i < n; i++)
+      if (str1[i] == 'T') {
+        if (ans.substr(i, m) != str2) return "";
+      }
+
+    for (char &c: ans) {
+      if (c == 0) c = 'a';
+    }
+
+    for (int i = 0; i < n; i++)
+      if (str1[i] == 'F' && ans.substr(i, m) == str2) {
+        bool failed = true;
+        for (int j = m - 1; j >= 0; j--)
+          if (!flag[i + j]) {
+            ans[i + j] = 'b';
+            failed = false;
+            break;
+          }
+
+        if (failed) return "";
+      }
+    return ans;
+  }
+};
